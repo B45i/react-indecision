7c0089b3504bddd961923738d87b643e8d6f03c2
B45i <amalshajan2011@gmail.com>
2019-05-13 22:31:08 +0530

    2 files modified, 2 added, 1 deleted

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 import IndecisionApp from './components/IndecisionApp'
+import './styles/styles.scss'
 
 render(<IndecisionApp/>, document.getElementById('root'));
